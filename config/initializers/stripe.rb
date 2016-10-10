5d19e6e89c1aa86b4e0e7d6b3a92ad8a10379690
simposk <simposk@gmail.com>
2016-10-10 18:47:07 +0300

    config stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,12 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_WMG2ytqLR8qQUQb1cdKANinp',
-  :secret_key      => 'sk_test_FqahU6I6h5AGG0JuXXJ1cGrH'
-}
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_WMG2ytqLR8qQUQb1cdKANinp',
+    :secret_key      => 'sk_test_FqahU6I6h5AGG0JuXXJ1cGrH'
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_WMG2ytqLR8qQUQb1cdKANinp',
+    :secret_key      => 'sk_test_FqahU6I6h5AGG0JuXXJ1cGrH'
+  }
 end
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
